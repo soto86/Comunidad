1c48162cbf7989fadd3569b2d0d6b42232a2a23b
santiagosoto <santiago_Soto86@yahoo.com.ar>
2020-05-15 18:53:28 -0300

    End of Section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -22,6 +22,14 @@ namespace API
         {
             services.AddDbContext<DataContext>(opt =>
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod()
+                    .WithOrigins("http://localhost:3000");
+                });
+            });
             services.AddControllers();
         }
 
@@ -38,6 +46,8 @@ namespace API
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseCors("CorsPolicy");
 
             app.UseEndpoints(endpoints =>
             {
